05ba2d0eb6a7864ef4f734a39a8f55d404ceb84c
sandeepgaur98223 <sandeepgaur98223@gmail.com>
2023-04-06 19:30:33 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider,connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
-//import {robots} from './robots';
+import { searchRobots } from './reducers';
 import 'tachyons';
 //as it exports more than 1 result. i.e. doesn't export only 1 value by default.
 
+const store=createStore(searchRobots);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
